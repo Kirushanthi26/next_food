6b965fbc5aa99888cdebfa22c68ac5ac5b4b553b
Kirushanthi26 <kirushanthikiru26@gmail.com>
2025-04-09 15:46:02 +0530

    refactor(mainHeader): move header background to separate component
    Move the header background styles and SVG markup from the global CSS and layout file into a dedicated `MainHeaderBackground` component to improve code organization and maintainability

--- components/mainHeader/main-header.tsx
+++ components/mainHeader/main-header.tsx
@@ -2,25 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import styles from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        <Image src={logoImg} alt="next logo" width={72} height={72} priority />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image
+            src={logoImg}
+            alt="next logo"
+            width={72}
+            height={72}
+            priority
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
