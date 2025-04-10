5ecd0e047acb35d4bb2856a6362bc9f3a0fb5765
Kirushanthi26 <kirushanthikiru26@gmail.com>
2025-04-10 13:39:07 +0530

    refactor(mainHeader): extract nav-link component for better maintainability. useclient

---
+++ components/mainHeader/nav-link.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+import styles from "./nav-link.module.css";
+
+type NavLinkProps = {
+  href: string;
+  children: ReactNode;
+};
+export default function NavLink({ href, children }: NavLinkProps) {
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={
+        path.startsWith(href) ? `${styles.link} ${styles.active}` : styles.link
+      }
+    >
+      {children}
+    </Link>
+  );
+}
